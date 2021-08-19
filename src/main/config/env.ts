fa94aff29f5216fa1ff397484d8ae9130fef2c52
Daniel Gonçalves Lopes <danielopesgl05@gmail.com>
2021-08-18 23:31:51 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://admin:mongodb@clean-node-api.kwksk.mongodb.net/clean-node?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5hH'
 }
