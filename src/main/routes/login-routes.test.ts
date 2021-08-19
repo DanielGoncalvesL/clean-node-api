aa80d22d29e2a19fd96381d7ee0ee2c5278f16ba
Daniel Gonçalves Lopes <danielopesgl05@gmail.com>
2021-08-18 23:40:34 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('Login routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 200 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'danielopesgl05@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
